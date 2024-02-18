581b18bfbac295eb42b90ac1e40bafa3a5d7cbfc
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2024-02-18 15:10:44 +0100

    Create front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>rbnu-mailforms</h1>
+<p>A service to send emails directly from static html forms.</p>
+<p>
+  This project is currently not open to the public, but I am planning to release a public version in
+  June of this year. Currently, it is being used for contact forms on client's websites.
+</p>
+<p>
+  You may self-host it with zero guarantees. The source code is available under the <a
+    href="https://www.gnu.org/licenses/gpl-3.0.nl.html">GPL GNUv3 license</a
+  >
+  on <a href="https://github.com/rubenuijtdewilligen/mailforms">GitHub</a>.
+</p>
+<p>Inspired by <a href="https://staticforms.xyz/">Static Forms</a>.</p>
+<hr />
+<small>©️ Ruben Uijtdewilligen</small>
