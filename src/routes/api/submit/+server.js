546b31dcf83258c2a334636212f2c4caeb40ef17
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2024-02-18 14:57:49 +0100

    Allow custom fields using a $ sign in front & rename accessToken to accessKey

--- src/routes/api/submit/+server.js
+++ src/routes/api/submit/+server.js
@@ -5,29 +5,45 @@ import mailer from '$lib/mailer';
 export const POST = async ({ request, locals }) => {
   const formData = await request.formData();
 
-  try {
-    const token = await locals.pb.collection('tokens').getOne(formData.get('accessToken'));
+  let customValues = [];
+  for (const [name, value] of formData.entries()) {
+    if (name.startsWith('$')) {
+      const customName = name.slice(1);
+      customValues.push({ name: customName, value });
+    }
+  }
 
-    let mailto = `mailto:${token.email}`;
+  try {
+    const token = await locals.pb.collection('tokens').getOne(formData.get('accessKey'));
+
+    let mailto = `mailto:${formData.get('email')}`;
     mailto += `?subject=RE: ${formData.get('subject')}`;
     const mailtoBody = `
----
-${formData.get('replyExplanation') || 'You are receiving this message because you filled out a form on our website.'}
-${formData.get('originalMessage') || 'Your original message:'}
+    ---
+    ${formData.get('replyExplanation') || 'You are receiving this message because you filled out a form on our website.'}
+    ${formData.get('originalMessage') || 'Your original message:'}
 
-${formData.get('message')}
-`;
+    ${formData.get('message')}
+    `;
     mailto += `&body=${encodeURIComponent(mailtoBody)}`;
 
-    const html = `
+    let html = `
     ${formData.get('message').replace(/\n/g, '<br />')}
-
 
     <br /><br />
     ---
-    <p style="margin-top: -0.5em; margin-bottom: -0.5em;">You cannot reply to this email directly. To reply, click <a href="${mailto}">here</a>.</p>
-    <small>This email was sent automatically using <a href="https://mailforms.rbnu.nl/">rbnu-mailforms</a>, a service by <a href="https://rbnu.nl/">Ruben Uijtdewilligen</a>.</small>
+    <h2 style="margin-top: -0.5em; margin-bottom: -0.5em;">User data</h2>
+    <b>Email address:</b> ${formData.get('email')}<br />
     `;
+
+    customValues.forEach((customValue) => {
+      html += `<b>${customValue.name}:</b> ${customValue.value}<br />`;
+    });
+
+    html += `
+    <br />
+    <p style="margin-top: -0.5em; margin-bottom: -0.5em;">You cannot reply to this email directly. To reply, click <a href="${mailto}">here</a>.</p>
+    <small>This email was sent automatically using <a href="https://mailforms.rbnu.nl/">rbnu-mailforms</a>, a service by <a href="https://rbnu.nl/">Ruben Uijtdewilligen</a>.</small>`;
 
     mailer.sendMail({
       from: `"${formData.get('from')}" <${env.SMTP_USERNAME}>`,
